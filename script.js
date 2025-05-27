7fb7109fad08c1d247bef4aeee51fd0b7f7e969b
grantmauger15 <grantmauger15@gmail.com>
2025-05-27 08:37:37 -0700

    Implement playRound()

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let humanScore, computerScore = 0
+
 const getComputerChoice = () => {
     let choice = Math.floor(Math.random() * 3)
 
@@ -15,4 +17,43 @@ const getHumanChoice = () => {
     let humanChoice = prompt("Enter rock, paper, or scissors");
 
     return humanChoice;
+}
+
+const playRound = (humanChoice, computerChoice) => {
+    humanChoice = humanChoice.toLowerCase();
+
+    if(humanChoice == computerChoice) {
+        console.log("There is a tie! No one wins.");
+    }
+    else if(humanChoice == "scissors") {
+        if(computerChoice == "paper") {
+            console.log("You win! Scissors beats Paper.");
+            humanScore++;
+        }
+        else {
+            console.log("You lose! Rock beats Scissors.");
+            computerScore++;
+        }
+        
+    }
+    else if(humanChoice == "paper") {
+        if(computerChoice == "rock") {
+            console.log("You win! Paper beats Rock.");
+            humanScore++;
+        }
+        else {
+            console.log("You lose! Scissors beats Paper.");
+            computerScore++;
+        }
+    }
+    else {
+        if(computerChoice == "scissors") {
+            console.log("You win! Rock beats Scissors.");
+            humanScore++;
+        }
+        else {
+            console.log("You lose! Paper beats Rock.");
+            computerScore++;
+        }
+    }
 }
