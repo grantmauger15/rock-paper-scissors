09522097e4757590743c2ade0c41056bc3fa8e68
grantmauger15 <grantmauger15@gmail.com>
2025-05-27 08:14:51 -0700

    Implement getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+const getComputerChoice = () => {
+    let choice = Math.floor(Math.random() * 3)
+
+    switch(choice) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
